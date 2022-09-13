045e623ad3c9b6969ea6bea410eee8daa2321f53
fk-pixel <fthbtlkrt@gmail.com>
2022-09-13 13:35:44 +0200

    first commit for route structure

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"encoding/json"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct{
+	ID string `json:"id"`
+	Isbn string `json:"isbn"`
+	Title string `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director{
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/[id]", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/[id]", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/[id]", updateMovie).Methods("UPDATE")
+
+}
+
