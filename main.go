0b87c6d1ee60a0732ec58db96e04fc7e3664dc29
fk-pixel <fthbtlkrt@gmail.com>
2022-09-13 14:07:37 +0200

    added delete and getmovies function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
-import(
+import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"encoding/json"
@@ -10,28 +11,55 @@ import(
 	"github.com/gorilla/mux"
 )
 
-type Movie struct{
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
-type Director{
+type Director struct {
 	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
+	Lastname  string `json:"lastname"`
 }
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie()
+
+func createMovie()
+
+func deleteMovie(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	params := max.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1]...)
+			break
+		}
+	} 
+}
+
+func updateMovie()
+
+
 func main() {
 	r := mux.NewRouter()
+
+	movies = append(movies, Movie{{ID: "1", Isbn: "123456789", Title: "First Movie", Director: &Director{Firstname: "Adam", Lastname: "Markdown"}})
+	movies = append(movies, Movie{{ID: "2", Isbn: "012345678", Title: "Second Movie", Director: &Director{Firstname: "Kurt", Lastname: "Agent"}})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/[id]", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/[id]", deleteMovie).Methods("DELETE")
-	r.HandleFunc("/movies/[id]", updateMovie).Methods("UPDATE")
+	r.HandleFunc("/movies/[id]", updateMovie).Methods("PUT")
 
+	fmt.Print("Starting server at port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
-
